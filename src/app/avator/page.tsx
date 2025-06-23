f61b7d788fe61123bd1c5d43f0d3b89434b1c149
Kenjiro56 <hiroken506@gmail.com>
2025-06-23 14:12:55 +0900

    ファイルを整理した

---
+++ src/app/avator/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AvatorPage = () => {
+  return (
+    <div>
+      <h1>Avator Page</h1>
+      <p>This is the avator page content.</p>
+    </div>
+  );
+}
+
+export default AvatorPage;
---
+++ src/app/lesson/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LessonPage = () => {
+  return (
+    <div>
+      <h1>Lesson Page</h1>
+      <p>This is the lesson page content.</p>
+    </div>
+  );
+}
+
+export default LessonPage;
