141f7f1b2c84dba8184efe128e8d21ce6d62bc3c
Kenjiro56 <hiroken506@gmail.com>
2025-06-29 18:57:37 +0900

    feat/serviceLogo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image'
 
 const Header = () => {
   return (
     <header className="bg-blue-600 text-white p-4">
       <div className="container mx-auto flex justify-center items-center">
-        <Link href="/" className="text-2xl font-bold hover:underline cursor-pointer">
-          GCP Study App
+        <Link href="/" className="cursor-pointer">
+          <Image
+            src="/serviceLogo.svg"
+            alt="Manabo Logo"
+            width={250}
+            height={60}
+            className="h-10 w-auto"
+            priority
+          />
         </Link>
       </div>
     </header>
