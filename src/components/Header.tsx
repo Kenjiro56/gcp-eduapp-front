be2dbbb02bb94b59c9e8c2418773ffd3060795a1
Kenjiro56 <hiroken506@gmail.com>
2025-06-29 18:32:40 +0900

    fix/serviceIcon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,20 +5,9 @@ const Header = () => {
   return (
     <header className="bg-blue-600 text-white p-4">
       <div className="container mx-auto flex justify-center items-center">
-        <h1 className="text-2xl font-bold">GCP Study App</h1>
-        {/* <nav> */}
-          {/* <ul className="flex space-x-4"> */}
-            {/* <li>
-              <Link href="/" className="hover:underline">Home</Link>
-            </li> */}
-            {/* <li>
-              <Link href="/avator" className="hover:underline">Avator</Link>
-            </li> */}
-            {/* <li>
-              <Link href="/lesson" className="hover:underline">Lesson</Link>
-            </li> */}
-          {/* </ul> */}
-        {/* </nav> */}
+        <Link href="/" className="text-2xl font-bold hover:underline cursor-pointer">
+          GCP Study App
+        </Link>
       </div>
     </header>
   );
